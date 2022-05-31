951efdb54b63ca54bf66cecfcc86b575e70117e7
Florenz Austria <florenzdalangin@gmail.com>
2022-05-31 17:25:29 -0400

    add tracks

---
+++ src/sections/Tracks.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Tracks.css"
+
+export default function Tracks() {
+
+    return (
+        <div id="tracks">
+            <h1>Tracks</h1>
+        </div>
+    )
+}
