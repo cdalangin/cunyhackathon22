9da19c7fca58800b33036eb7d409dd46661931c1
huilin-zou <huilin.zou@cix.csi.cuny.edu>
2022-06-19 23:19:16 -0400

    FEAT-added mentor page

--- src/pages/Mentor.js
+++ src/pages/Mentor.js
@@ -1,10 +1,38 @@
-import React from "react"
-import '../styles/Mentor.css'; 
+import React from "react";
+import "../styles/Mentor.css";
+import clipboard from "../assets/clipboard.png";
+import Nav from "../sections/Nav";
+import { Link } from "react-router-dom";
+import applyNow from "../assets/applyNow.png";
 
 export default function Mentor() {
+  const mentorText = [
+    {
+      id: 1,
+      name: "mentor",
+      icon: clipboard,
+      description:
+        "A mentor will support student hackers in their project by answering their questions. These questions will range from idea validation, design, programming, pitching, and more. You have the choice of what timeslots you will be available for.",
+      title: "Mentor Application",
+    },
+  ];
   return (
     <div>
-      <h1>Mentor Page</h1>
+     <div className="main" style={{ minHeight: "100vh" }}>
+        <Nav />
+        {mentorText.map((mentor) => {
+          return (
+            <div className="mentor-box">
+              <h3 className="mentor-title">{mentor.title}</h3>
+              <img src={mentor.icon} alt={mentor.name} className="mentor-icon" />
+              <p className="mentor-desc">{mentor.description}</p>
+            </div>
+          );
+        })}
+        <Link to="/mentor">
+          <img src={applyNow} alt="apply now icon" className="apply-now" />
+        </Link>
+      </div>
     </div>
   );
 }
