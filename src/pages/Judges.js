71560c56803d9db889275c43249c6af0ec6e46b8
Cirill <40480780+cdalangin@users.noreply.github.com>
2022-06-20 22:17:59 -0400

    Revert "Mentor"

--- src/pages/Judges.js
+++ src/pages/Judges.js
@@ -1,41 +1,10 @@
-import React from "react";
-import "../styles/Main.css";
-import Nav from "../sections/Nav";
-import clipboard from "../assets/clipboard.png";
-import applyNow from "../assets/applyNow.png";
-import { Link } from "react-router-dom";
- 
+import React from "react"
+import '../styles/Judges.css'; 
+
 export default function Judges() {
-  const judgeText = [
-    {
-      id: 1,
-      name: "judge",
-      icon: clipboard,
-      description:
-        "A judge will review and assess the projects made during the hackathon near the end of the event, tentatively set for 8am - 12pm on Sunday Nov 14, 2021. Judges will use Devpost and the judging criteria and guidelines will be provided to make it as easy and efficient as possible.",
-      title: "Judge Application",
-    },
-  ];
- 
   return (
     <div>
-      <div className="main" style={{ minHeight: "100vh" }}>
-        <Nav />
-        {judgeText.map((judge) => {
-          return (
-            <div className="judge-box">
-              <h3 className="judge-title">{judge.title}</h3>
-              <img src={judge.icon} alt={judge.name} className="judge-icon" />
-              <p className="judge-desc">{judge.description}</p>
-            </div>
-          );
-        })}
-        <Link to="/mentor">
-          <img src={applyNow} alt="apply now icon" className="apply-now" />
-        </Link>
-      </div>
+      <h1>Judges Page</h1>
     </div>
   );
 }
- 
-
--- src/pages/Mentor.js
+++ src/pages/Mentor.js
@@ -1,41 +1,10 @@
-import React from "react";
-import "../styles/Mentor.css";
-import clipboard from "../assets/clipboard.png";
-import Nav from "../sections/Nav";
-import { Link } from "react-router-dom";
-import applyNow from "../assets/applyNow.png";
+import React from "react"
+import '../styles/Mentor.css'; 
 
 export default function Mentor() {
-  const mentorText = [
-    {
-      id: 1,
-      name: "mentor",
-      icon: clipboard,
-      description:
-        "A mentor will support student hackers in their project by answering their questions. These questions will range from idea validation, design, programming, pitching, and more. You have the choice of what timeslots you will be available for.",
-      title: "Mentor Application",
-    },
-  ];
   return (
     <div>
-      <Nav />
-     <div id="mentor" className="main">
-        
-        {mentorText.map((mentor) => {
-          return (
-            <div className="mentor-box">
-              <div className="mentor-text">
-              <h3 className="mentor-title">{mentor.title}</h3>
-              <p className="mentor-desc">{mentor.description}</p>
-              <Link to="/mentor">
-                <img src={applyNow} alt="apply now icon" className="apply-now" />
-              </Link>
-              </div>
-            </div>
-          );
-        })}
-        
-      </div>
+      <h1>Mentor Page</h1>
     </div>
   );
 }
